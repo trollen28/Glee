476502bd0cbc1802ee076cc859e26c137a9b5aca
trollen28 <wmz1234@gmail.com>
2021-05-25 14:31:36 +0500

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    $('.slider__inner').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpead: 5000,
+    });
 });
