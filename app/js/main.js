96fd2e67e625ecc68d3abca9fba90fce9da1b3b1
trollen28 <wmz1234@gmail.com>
2021-08-27 17:51:39 +0500

    shop page done

--- app/js/main.js
+++ app/js/main.js
@@ -22,6 +22,18 @@ $(function () {
         $('.mobile__btn').toggleClass('mobile__btn--active');
     });
 
+    $('.shop__grid').on('click', function () {
+        $('.shop__btn-grid').addClass('shop__btn-grid--active');
+        $('.shop__btn-list').removeClass('shop__btn-list--active');
+        $('.shop__items').removeClass('shop__items--list');
+    });
+
+    $('.shop__list').on('click', function () {
+        $('.shop__btn-list').addClass('shop__btn-list--active');
+        $('.shop__items').addClass('shop__items--list');
+        $('.shop__btn-grid').removeClass('shop__btn-grid--active');
+    });
+
     $('.main-slider__inner').slick({
         dots: true,
         arrows: false,
@@ -68,7 +80,14 @@ $(function () {
         ratedFill: "#ffcc00",
         readOnly: true,
         spacing: "5px",
-        
+    });
+
+    $(".product-card__stars").rateYo({
+        starWidth: "19px",
+        normalFill: "#e0e0e0",
+        ratedFill: "#ffcc00",
+        readOnly: true,
+        spacing: "13px",
     });
 
     var containerEl1 = document.querySelector('[data-ref="container-1"]');
